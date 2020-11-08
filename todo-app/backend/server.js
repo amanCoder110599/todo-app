d7c6926e20674bb6d0d0f64d09d4f90345a00316
amanCoder110599 <physicsaman110599@gmail.com>
2020-11-08 23:04:23 +0530

    Add all files

--- todo-app/backend/server.js
+++ todo-app/backend/server.js
@@ -11,7 +11,8 @@ app.use(cors());
 app.use(express.json());
 app.use(expressip().getIpInfoMiddleware);
 
-const uri = process.env.ATLAS_URI;
+const uri =
+  "mongodb+srv://schools:GC00eB1WtiM34a6D@schools.l9qgr.mongodb.net/<dbname7>?retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 const connection = mongoose.connection;
 connection.once("open", () => {
